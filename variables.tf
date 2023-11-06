59344ad62f0bca7fdec863cbcfc4e4f2afbf2b00
vyashya12 <vyashyaraj25@gmail.com>
2023-11-06 14:26:56 +0800

    Added default will remove and change soon

--- variables.tf
+++ variables.tf
@@ -2,11 +2,13 @@ variable "vcd_url" {
   description = "vcd URL"
   type        = string
   sensitive   = true
+  default     = "https://my1-dashboard.exabytes.cloud/api"
 }
 variable "api_token" {
   description = "API token"
   type        = string
   sensitive   = true
+  default     = "Sn1bTTML83TDlz6kzLqIy3Mcr8e6QWu2"
 }
 
 variable "vm_name" {
